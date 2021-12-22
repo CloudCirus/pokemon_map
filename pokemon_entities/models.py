dc56d7ee31829c096814efc4dfef1abfc6fa96eb
Cloud <smirnovceo@yandex.ru>
2021-12-22 19:12:37 +0500

    Ad model field Pokemon.image

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name='Имя', max_length=200)
+    image = models.ImageField(verbose_name='Изображение', null=True)
 
     def __str__(self) -> str:
         return f'{self.title}'
